1c7bb9aee4973a59aa231a710b6fb32dd1a9cfc7
Ryan Shee <rshee@lumoslabs.com>
2019-01-26 18:55:56 -0800

    Music test

---
+++ Magic Movers/Assets/AirConsole/examples/Magic Movers/scripts/MusicTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTest : MonoBehaviour
+{
+    public Camera camera;
+    private FMODUnity.StudioEventEmitter emitter;
+    private float value = 0;
+
+    void Start()
+    {
+        emitter = camera.GetComponent<FMODUnity.StudioEventEmitter>();
+        Debug.Log("emitter: " + emitter);
+    }
+
+    void Update()
+    {
+        value += 0.001f;
+        Debug.Log(value);
+        emitter.SetParameter("gameEnd", value);
+    }
+}
